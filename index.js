119a11945b10d970445971c16957edc0dc34fa42
manishsaini511 <54799227+manishsaini511@users.noreply.github.com>
2021-10-01 14:19:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ var numberOFDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0 ; i<numberOFDrumButtons ; i++) { 
 
     document.querySelectorAll(".drum") [i].addEventListener("click", function () {
-        this.style.color ="white";
+
+        var buttonInnerHtml = this.innerHTML;
+        
+        switch (buttonInnerHtml) {
+            case "w": 
+            var tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play();
+            break;
+
+            case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3")
+            tom2.play(); 
+            break;
+            
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play();
+            default:
+                break;
+        }
+
     } );
 }
 
